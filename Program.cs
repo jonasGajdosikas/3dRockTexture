44eb136d0ae1620c8079a068ad5a08dd87198a3d
jogajdosik <jogajdosik@gmail.com>
2021-10-11 14:34:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3dRockTexture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
